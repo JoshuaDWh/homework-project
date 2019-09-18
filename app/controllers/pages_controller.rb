bc62dc0cad0201681a82bc869545b11e03614732
Joshua White <whitejoshua99@yahoo.com>
2019-09-18 09:30:21 -0500

    Initial Commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,14 @@ class PagesController < ApplicationController
       format.html { render :review }
     end
   end
+  def question
+    features = [
+      "a) A class should have only one reason to change.",
+      "b) Each developer on a team should have one main responsibiliy.",
+      "c) Every app will have one responsibility that is most important to users."
+    ]
+    respond_to do |format|
+      format.html { render :question, locals: { features: features } }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'jwhite31', to: 'pages#jwhite31', as: 'jwhite31'
   #<%= image_tag "quiz-bubble.png", height: 300 %>
   get 'review', to: 'pages#review', as: 'review'
-  root to: redirect('/jwhite31', status: 302)
+  get 'question', to: 'pages#question', as: 'question'
+  root to: redirect('/question', status: 302)
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
