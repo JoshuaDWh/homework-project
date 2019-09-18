dcf6d713c996e0b6f9be274f53c576d6e63a373f
Joshua White <whitejoshua99@yahoo.com>
2019-09-18 09:54:04 -0500

    Initial commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,13 +10,16 @@ class PagesController < ApplicationController
     end
   end
   def question
-    features = [
+    q = [
+      "Which of the following is the Single Responsibility Principle?"
+    ]
+    answers = [
       "a) A class should have only one reason to change.",
       "b) Each developer on a team should have one main responsibiliy.",
       "c) Every app will have one responsibility that is most important to users."
     ]
     respond_to do |format|
-      format.html { render :question, locals: { features: features } }
+      format.html { render :question, locals: { q: q, answers: answers } }
     end
   end
 end
